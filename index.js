184f5bd0803185656551a7db0697ab537e646d8b
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-14 12:30:58 -0400

    commit tree fb105f66057b58862121fc0834107789ff4f2976

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+
+  function superbowlWin(record) {
+      let theYear = record.find(findTheYear);
+      //console.log(theYear);
+      if (theYear === undefined) { return undefined; }
+      else { return theYear.year; }
+
+  }
+
+
+function findTheYear(year) {
+    if (year.result === "W") { return true; }
+}
+  
+
+//console.log(superbowlWin(record));
